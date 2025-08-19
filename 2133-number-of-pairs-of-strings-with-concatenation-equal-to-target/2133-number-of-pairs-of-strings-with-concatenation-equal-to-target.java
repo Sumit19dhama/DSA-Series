eeb1534704efca0ac9ec60294910376281eced61
SUMIT DHAMA <144706400+Sumit19dhama@users.noreply.github.com>
2025-08-19 11:59:50 +0530

    Time: 27 ms (50.52%), Space: 45.1 MB (50.26%) - LeetHub

---
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numOfPairs(String[] nums, String target) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(i != j){
+                    String curr = nums[i] + nums[j];
+                    if(curr.equals(target)){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
