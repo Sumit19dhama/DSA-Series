e34cae7516643cb8acdc7165dcbea72328774532
SUMIT DHAMA <144706400+Sumit19dhama@users.noreply.github.com>
2025-08-24 21:51:17 +0530

    Time: 64 ms (77.06%), Space: 62.5 MB (6.93%) - LeetHub

---
+++ 3265-maximum-good-subarray-sum/3265-maximum-good-subarray-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        Map<Integer, Long> valToMinPrefixSum = new HashMap<>();
+        long maxSum = Long.MIN_VALUE, prefixSum = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            // Ensure we store the *earliest (smallest)* prefix sum for nums[i]
+            if (valToMinPrefixSum.getOrDefault(nums[i], Long.MAX_VALUE) > prefixSum) {
+                valToMinPrefixSum.put(nums[i], prefixSum);
+            }
+
+            // Update running prefix sum
+            prefixSum += nums[i];
+
+            // Case 1: subarray starts with (nums[i] + k)
+            if (valToMinPrefixSum.containsKey(nums[i] + k)) {
+                maxSum = Math.max(maxSum, prefixSum - valToMinPrefixSum.get(nums[i] + k));
+            }
+
+            // Case 2: subarray starts with (nums[i] - k)
+            if (valToMinPrefixSum.containsKey(nums[i] - k)) {
+                maxSum = Math.max(maxSum, prefixSum - valToMinPrefixSum.get(nums[i] - k));
+            }
+        } 
+
+        return maxSum == Long.MIN_VALUE ? 0 : maxSum;        
+    }
+
+}
